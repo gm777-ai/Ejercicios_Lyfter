a79dd577a26c4ec84a5025adc353c409794bfc41
GwenM <gwen_miranda@hotmail.es>
2026-05-22 00:06:43 -0700

    radius attribute defined in __init(self, radius)

--- Python Intermedio/Ejercicios de OPP/Ejercicio1.py
+++ Python Intermedio/Ejercicios de OPP/Ejercicio1.py
@@ -1,12 +1,14 @@
 # Calculating the area of a circle
 
 class Circle:
-    radius = 4
+    def __init__(self,radius):
+        self.radius = radius
+        return
 
     def get_area(self):
         self.area = 3.141516 * (self.radius ** 2) 
         print(f" The radius is {self.radius}")
         return self.area
 
-area = Circle()
+area = Circle(4)
 print(f"The area of a circle with radius {area.radius} is {area.get_area()}") 
