2185a7905c18f415d2209e6d0535114df47ede12
GwenM <gwen_miranda@hotmail.es>
2026-06-03 23:59:17 -0700

    Ejercicio 2 arreglado

--- Python Intermedio/Algoritmos de Ordenamiento/Ejercicios de Algoritmos de Ordenamiento/Ejercicio2.py
+++ Python Intermedio/Algoritmos de Ordenamiento/Ejercicios de Algoritmos de Ordenamiento/Ejercicio2.py
@@ -2,11 +2,11 @@ def bubble_sort(numbers):
     n = len(numbers)
 
     for i in range(n):
-        for j in range(0, n - 1 - i):
-            if numbers[j] < numbers[j + 1]:
-                temporary = numbers[j]
-                numbers[j] = numbers[j + 1]
-                numbers[j + 1] = temporary
+        for j in range(n - 1, i, -1):
+            if numbers[j] < numbers[j - 1]:
+                temp = numbers[j]
+                numbers[j] = numbers[j - 1]
+                numbers[j - 1] = temp
 
     return numbers
 
