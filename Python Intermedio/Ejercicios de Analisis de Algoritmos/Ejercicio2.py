4cb102fde5cc092f93d9e1bda06765d84e77460d
GwenM <gwen_miranda@hotmail.es>
2026-06-06 02:15:13 -0700

    Analisis de Algoritmos 1 y 2

---
+++ Python Intermedio/Ejercicios de Analisis de Algoritmos/Ejercicio2.py
@@ -0,0 +1,13 @@
+"""
+
+2. Para Complejidad de tiempo = CT y complejidad de espacio = CE
+
+print_numbers_times_2 : CT = O(n)  y CE = O(1)
+
+check_if_lists_have_an_equal: CT = O(1) y CE = O(n²)
+
+print_10_or_less_elements: CT = O(1) y CE = O(1)
+
+generate_list_trios: CT = O(n³) y CE = O(n³)
+
+"""
